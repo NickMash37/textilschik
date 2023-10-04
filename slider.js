b0791389e5f9f32e000cc646a1eb373bbf059dd9
Nikolay <areyfclm@gmail.com>
2023-10-04 18:32:14 +0300

    main section and slider start work

--- slider.js
+++ slider.js
@@ -1,15 +1,18 @@
-$(document).ready(function(){
-    $("#slider").owlCarousel({
-        loop: true,
+jQuery(document).ready(($) => {
+    $('.main-slider').owlCarousel({
+        items: 1,
+        margin: 10,
         nav: true,
+        loop: true,
+        dots: true,
+        autoplay: true,
         responsive: {
-            0:{
+            0: {
                 items: 1
             },
-
             1000: {
                 items: 1
             }
         }
     });
-  });
+})
