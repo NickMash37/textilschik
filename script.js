c43f5e9101c45805a1c192cc9b0e32f6782189d1
Nikolay <areyfclm@gmail.com>
2023-10-05 20:40:09 +0300

    fixtures block tabs released

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ $(".single-item").slick({
     dots: true,
     infinite: true,
     autoplay: true,
-    speed: 1500,
+    speed: 400,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    arrows: false
   });
+
+  // JS scripts
+
+  const prevBtn = document.querySelector('.prev')
+  const nextBtn = document.querySelector('.next')
+  const prevFixtures = document.querySelector('.previous-fixture')
+  const upcomingFixtures = document.querySelector('.upcoming-fixtures')
+
+  prevBtn.addEventListener('click', () => {
+    prevFixtures.classList.remove('disabled')
+    upcomingFixtures.classList.add('disabled')
+  })
+
+  nextBtn.addEventListener('click', () => {
+    upcomingFixtures.classList.remove('disabled')
+    prevFixtures.classList.add('disabled')
+  })
